0d0bd06b917303432a2e5a12fb94a10cd14e0ffc
TheDragonsNest <vinweinmann@icloud.com>
2023-09-29 20:48:51 +0200

    functional lzw decompression

--- LZW.py
+++ LZW.py
@@ -1,10 +1,10 @@
-def compress(message):
+def compress(txt):
     dictionary = {chr(i): i for i in range(256)}
     next_code = 256
     result = []
-
     current_word = ""
-    for c in message:
+
+    for c in txt:
         current_word += c
         if current_word not in dictionary:
             dictionary[current_word] = next_code
@@ -15,3 +15,26 @@ def compress(message):
     result.append(dictionary[current_word])
 
     return result
+
+
+def decompress(txt):
+    dictionary = {i: chr(i) for i in range(256)}
+    next_code = 256
+    result = []
+    current_word = ""
+
+    for code in txt:
+        if code in dictionary:
+            word = dictionary[code]
+        elif code == next_code:
+            word = current_word + current_word[0]
+        else:
+            raise ValueError("Code out of Bounds. Invalid input")
+
+        result.extend(word)
+        dictionary[next_code] = current_word + word[0]
+        next_code += 1
+
+        current_word = word
+
+    return ''.join(result)
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+import re
+
 import LZW
 
 if __name__ == "__main__":
-    txt = input("Text to compress: ")
-    compressed_text = LZW.compress(txt)
-    ascii_text = [ord(char) for char in txt]
+    choice = input("compress or decompress??")
+    if choice == "compress":
+        txt = input("Text to compress: ")
+        compressed_text = LZW.compress(txt)
+        ascii_text = [ord(char) for char in txt]
 
-    print("ASCII: ", ascii_text)
-    print("LZW:   ", compressed_text)
-    print(f"LZW is {((len(compressed_text) / len(ascii_text)) * 100):.1f}% of ASCII")
+        print("ASCII: ", ascii_text)
+        print("LZW:   ", compressed_text)
+        print(f"LZW is {((len(compressed_text) / len(ascii_text)) * 100):.1f}% of ASCII")
+    elif choice == "decompress":
+        txt = input("Compressed Text: ")
+        txt = re.sub('[,.-]', "", txt)
+        print("Decompressed Text: ", LZW.decompress([int(code) for code in re.split('\s+', txt)]))
