40f10043addc4ae421f0c675ecea5bf603025b23
weinmavi <vinweinmann@icloud.com>
2023-10-13 13:00:37 +0200

    argparse initial

--- GUI.py
+++ GUI.py
@@ -1,14 +1,25 @@
 import tkinter as tk
+
+
 class GUI:
     def __init__(self):
-        self.frame = tk.Frame()
+        self.window = tk.Tk()
+        self.panel_amount
+        for i in range(panel_amount):
+            GUI_Frame.pack()
 
 
-
-
-
-class GUI_Panel:
+class GUI_Frame(tk.Frame):
     def __init__(self, text):
+        super().__init__()
         self.label = tk.Label(text)
         self.entry = tk.Entry()
         self.button = tk.Button()
+
+
+if __name__ == '__main__':
+    window = tk.Tk()
+    label = tk.Label(text="Name")
+    entry = tk.Entry()
+    label.pack()
+    entry.pack()
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import re
+import argparse as ap
 
 import Huffman
 import LZW
 
 if __name__ == "__main__":
+    parser = ap.ArgumentParser()
+    compression_group = parser.add_mutually_exclusive_group()
+    compression_group.add_argument("-h", "--huff", "--huffman", action="store_true", help="Use Huffman")
+    compression_group.add_argument("-l", "--lzw", action="store_true", help="Use LZW")
+    direction_group = parser.add_mutually_exclusive_group()
+    direction_group.add_argument("-d", "--decompress", action="store_true")
+    direction_group.add_argument("-c", "--compress", action="store_true")
+    parser.add_argument()
+
     choice = input("compress or decompress??")
     if choice == "compress":
         txt = input("Text to compress: ")
@@ -17,3 +27,4 @@ if __name__ == "__main__":
         txt = input("Compressed Text: ")
         txt = re.sub('[,.-]', " ", txt)
         print("Decompressed Text: ", LZW.decompress([int(code) for code in re.split(' +', txt)]))
+
